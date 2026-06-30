44c8a1eb433a30584259e326b2809e9eef46d90d
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2026-06-30 17:12:26 +0530

    float min/max  ,  double min/mX

--- Variables.java
+++ Variables.java
@@ -10,7 +10,13 @@ class Variables{
 
   //used for storing decimal numbers
     float e = 50.5f; //ranges from 1.4e-45 to 3.4028235e38 and requires an 'f' at the end of the number to indicate that it is a float and round to 7 decimal places
+
+    float floatMin = Float.MIN_VALUE; //1.4e-45
+    float floatMax = Float.MAX_VALUE; //3.4028235e38
+
     double f = 60.6; //ranges from 4.9e-324 to 1.7976931348623157e308
+    double doubleMin = Double.MIN_VALUE; //4.9e-324
+    double doubleMax = Double.MAX_VALUE; //1.7976931348623157e308
 
     //used for storing single characters
     char g = 'A'; //ranges from 0 to 65,535
