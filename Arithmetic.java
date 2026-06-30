ed02d9daeaaefa9a2d4aa02cab6b0292cad9e167
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2026-06-30 21:09:45 +0530

    arithmetic operators

---
+++ Arithmetic.java
@@ -0,0 +1,14 @@
+public class Arithmetic {
+  public static void main(String[]args){
+    int a  =5;
+    int b  = 10;
+    int division  = b/a;
+    System.out.println("division of a and b is: " + division);
+
+
+    int x = 5;
+    double y = 123.2;
+    double sum=  x + y;
+    System.out.println("sum of x and y is: " + sum);
+  }
+}
