b2601aeccee20caaa26d0fd1905b2c0117e2cec7
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2026-06-30 17:09:58 +0530

    variables

--- Variables.java
+++ Variables.java
@@ -9,7 +9,7 @@ class Variables{
 
 
   //used for storing decimal numbers
-    float e = 50.5f; //ranges from 1.4e-45 to 3.4028235e38
+    float e = 50.5f; //ranges from 1.4e-45 to 3.4028235e38 and requires an 'f' at the end of the number to indicate that it is a float and round to 7 decimal places
     double f = 60.6; //ranges from 4.9e-324 to 1.7976931348623157e308
 
     //used for storing single characters
