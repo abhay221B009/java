5d5beed81f4ff645646d08c6c31b827c06458526
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2026-06-30 16:58:15 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,13 @@
+class Test{
+  public static void main(String []args){ //public static void main(String []args) is the entry point of the program
+    //public : The public keyword is an access modifier, which allows the main method to be accessible from anywhere in the program.
+    //static : The static keyword allows the main method to be called without creating an instance of the class. It belongs to the class itself rather than an instance of the class.
+    //void : The void keyword indicates that the main method does not return any value. 
+    //String []args : The String []args parameter allows the main method to accept command-line arguments as an array of strings. It enables users to pass input values to the program when executing it from the command line.
+    System.out.println("welcome abhay!, we are learning java");
+    //system.out.println() is a method in Java that is used to print output to the console. It is part of the java.lang package and is commonly used for displaying messages, debugging information, or any other output that needs to be shown to the user.
+    //system : The system is a built-in class in Java that provides access to system resources and functionalities. It contains various methods and fields for performing input/output operations, managing system properties, and interacting with the underlying operating system.
+    //out : The out is a static member of the System class that represents the standard output stream. It is an instance of the PrintStream class and is used to send output to the console.
+    //println() : The println() method is a member of the PrintStream class and is used to print a line of text to the console. It automatically adds a new line after the output, so subsequent output will appear on the next line.
+  }
+}
